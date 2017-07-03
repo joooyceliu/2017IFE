b60a28c204faab373581bbc99a16e74032975c8c
joooyceliu <1612808776@qq.com>
2017-07-03 21:59:50 +0800

    yytask6拖拽功能实现中

--- scripts/yytask6.js
+++ scripts/yytask6.js
@@ -7,20 +7,41 @@ var ok = document.getElementById("ok");
 var cancel = document.getElementById("cancel");
 var container = document.getElementById("container");
 
+var isDrag = false;
+//var windowW = window.innerWidth;   //浏览器窗口宽度
+//var windowH = window.innerHeight;   //浏览器窗口高度
+var e = e || window.event;
+
 btn.onclick = function(){
 	main.style.display = "block";
 };
 main.onclick = function(){
 	main.style.display = "none";
+	
 };
 container.onclick = function(){
-	
-	
 	main.style.display = "block";
 };
+
+
 ok.onclick = function(){
 	main.style.display = "none";
 };
 cancel.onclick = function(){
 	main.style.display = "none";
 };
+
+document.onmousemove = moveFunc;
+document.onmouseup = upFunc;
+function downFunc(){
+	isDrag = true;
+}
+function moveFunc(){
+	if(isDrag){
+		container.style.top = e.clientY;
+		container.style.left = e.clientX;
+	}
+}
+function upFunc(){
+	isDrag = false;
+}
